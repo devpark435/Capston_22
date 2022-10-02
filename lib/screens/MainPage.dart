3f38dad18abfab2bed4c04e09d2d2a8938b123e0
devpark435 <devpark435@gmail.com>
2022-10-02 23:38:32 +0900

    Add Tapbar&ListBox

---
+++ lib/screens/MainPage.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import '../../widgets/customWidget.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    //MARK: DefaultTabController
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        //MARK: Custom AppBar
+        appBar: AppBar(
+          title: const Text(
+            'Stock Application',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: false,
+          elevation: 0,
+          //MARK: bottom in tabBar
+          bottom: TabBar(
+            isScrollable: true,
+            indicatorWeight: 3,
+            tabs: <Widget>[
+              Tab(
+                child: Text("chart1"),
+              ),
+              Tab(child: Text("chart2")),
+              Tab(child: Text("chart3")),
+            ],
+          ),
+        ),
+        body:
+            //MARK: TabBarView
+            TabBarView(
+          children: <Widget>[
+            //MARK: Using Class ListContainer
+            Tab(
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 10, 0),
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.zero,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                      child: GestureDetector(
+                        //   onTap: () {
+                        //     Navigator.push(
+                        //         context,
+                        //         MaterialPageRoute(
+                        //             builder: (context) =>
+                        //                 ComparePage(title: 'ComparePage')));
+                        //   },
+                        child: ListBox(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Tab(
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 10, 0),
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.zero,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                      child: GestureDetector(
+                        //   onTap: () {
+                        //     Navigator.push(
+                        //         context,
+                        //         MaterialPageRoute(
+                        //             builder: (context) =>
+                        //                 ComparePage(title: 'ComparePage')));
+                        //   },
+                        child: ListBox(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
