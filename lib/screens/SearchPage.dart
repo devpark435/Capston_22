4b4e0b7bf11626c21f69f886edbe1285d94ac47d
devpark435 <devpark435@gmail.com>
2022-10-04 20:30:11 +0900

    Add SearchPage

---
+++ lib/screens/SearchPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../asset/palette.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key, required this.title});
+  final String title;
+  @override
+  State<SearchPage> createState() => _SearchPage();
+}
+
+class _SearchPage extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.bgColor,
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Palette.bgColor,
+              ),
+              child: Text(
+                "Stock Application",
+              ),
+            )
+          ]),
+    );
+  }
+}
